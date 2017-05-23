51b4d339c0ad5a8faf5d59cd041293f9d1bd95df
Rujiporn Thavornkanlapachai <rthavornkanl@unimelb.edu.au>
2017-05-23 15:09:42 +1000

    Remove comment

---
+++ strcalc2.R
@@ -0,0 +1,18 @@
+compute = function(input_string) {
+# Perform simple arithmetic encoded in input string
+# e.g. '1 + 2' -> 3, or '1 - 2' -> -1
+#Remove comment
+compute = function(input_string) {
+ values = unlist(strsplit(input_string, ' '))
+ num0 = as.integer(values[1])
+ operator = values[2]
+ num1 = as.integer(values[3])
+ result = switch(operator,
+                  '+' = num0 + num1,
+                  '-' = num0 - num1
+				  NA)
+  if (is.na(result)) {
+    print('unknown operator!')
+  }
+  return(result)
+}
